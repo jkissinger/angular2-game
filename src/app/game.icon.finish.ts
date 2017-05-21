0fa59583b49aae4b6df92b2fc7bf27e39c033bea
Jonathan Kissinger <kissi1j@cmich.edu>
2017-05-21 17:41:52 -0400

    Drawing finish line as a flag.

--- src/app/game.icon.finish.ts
+++ src/app/game.icon.finish.ts
@@ -6,9 +6,16 @@ export class FinishIcon extends GameIcon {
         super(minX, maxX, minY, maxY);
         this.color = "green";
         this.font = "Consolas - 20px";
-        this.height = 5;
-        this.width = 5;
+        this.height = 8;
+        this.width = 14;
         this.name = "FinishLine";
     }
 
+    draw(context: CanvasRenderingContext2D) {
+        context.font = this.font;
+        context.fillStyle = this.color;
+        context.fillRect(this.x, this.y, this.width, this.height);
+        let poleWidth = Math.floor(this.width / 5);
+        context.fillRect(this.x, this.y, poleWidth, this.height * 3);
+    }
 }
